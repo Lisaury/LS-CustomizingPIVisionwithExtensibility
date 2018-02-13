ff9214d4d71e1c68b82fc6d64744a13a605e9621
Lisaury <36410809+Lisaury@users.noreply.github.com>
2018-02-13 16:40:14 -0600

    Second commit

--- sym-barchart.js
+++ sym-barchart.js
@@ -25,8 +25,18 @@
 				The file sym-barchart-config.html was created to add the component for each property to show
 				*/
 				BackgroundColor: '#cdd0db',
-				BorderRadius: 10,
-				TextColor: 'rgb(0,255,0)'
+				BorderRadius: 0,
+				TextColor: 'rgb(0,255,0)',
+				depth: 0,
+				angle: 30,
+				columnWidth: 0.5,
+				includeElementName: true,
+				title: "",
+				text: "",
+				showCategoryAxisLabels: true,
+				useFixedAxisRange: false,
+				fixedYMin: 0,
+				fixedYMax: 1
 			} 
 		},
 		
@@ -41,6 +51,17 @@
 		}
 	}
 	
+	
+	
+	symbolVis.prototype.init = function(scope, elem) {
+		var labels;
+		var units;
+		var container= elem.find('#container')[0];
+		/* LS: Creating an unique ID for the graph*/
+		container.id="barchart_" + scope.symbol.Name;
+		
+		var chart = AmCharts.makeChart(container.id,getConfig());
+
 	/* LS:  Next code was taken for armchart to create a 3d graph. Some modifications were made to change some features.
 			This code indicate how the graph will be.
 	*/
@@ -48,8 +69,10 @@
 		return {
 			"type": "serial",
 			"categoryField": "attribute",
-			"angle": 30,
-			"depth3D": 30,
+			"columnWidth": scope.config.columnWidth,
+			/*"plotAreaFillAlphas": 1,*/
+			"depth":  scope.config.depth,
+			"angle":  scope.config.angle,
 			"colors": [
 				"#00A2E8"
 			],
@@ -57,8 +80,9 @@
 			"startDuration": 1,
 			"color": "#000000",
 			"categoryAxis": {
-			"autoGridCount": false,
+			/*"autoGridCount": false,*/
 			/* LS: Changing the label rotation to get a better fit of the labels */
+			"axisAlpha": 1,
 				"labelRotation": 45,
 				"gridPosition": "start"
 			},
@@ -70,7 +94,6 @@
 					/*"title": "General",*/
 					"type": "column",
 					"fillAlphas": 1,
-
 					"valueField": "value"
 				}
 			],
@@ -78,9 +101,15 @@
 			"valueAxes": [
 				{
 					"id": "ValueAxis-1",
-					"title": "Y Axis"
+					"title": scope.config.title
 				}
 			],
+			"categoryAxis": {
+                            "axisAlpha": 1,
+                             "gridAlpha": 1,
+							"autoWrap": true,
+							labelsEnabled: true
+						},
 			"allLabels": [],
 			"balloon": {},
 			"legend": {
@@ -91,7 +120,7 @@
 				{
 					"id": "Title-1",
 					"size": 15,
-					"text": "Comparison Barchart"
+					"text": scope.config.text
 				}
 			],
 			"dataProvider": [
@@ -109,40 +138,51 @@
 		}
 		
 	}
-	
-	
-	symbolVis.prototype.init = function(scope, elem) {
-		var labels;
-		var container= elem.find('#container')[0];
-		/* LS: Creating an unique ID for the graph*/
-		container.id="barchart_" + scope.symbol.Name;
-		
-		var chart = AmCharts.makeChart(container.id,getConfig());
-		
-		function convertToChart(data) {
+
+	function convertToChart(data,labels) {
 		
 		  return data.Rows.map(function(item,index){
 			var str=labels[index];
-			var n=str.indexOf("|");
+			/*var n=str.indexOf("|");*/
 			  return {
-
 				  value: item.Value,
-				  
-				 /* LS: Showing only the letters after "|" in the name.  */
-				  attribute: labels[index].slice(n+1)
-				 
+				  units: item.Units,
+				  attribute: labels[index].Label /*labels[index].slice(n+1)*/
 			  }
 			});
 			
 			
 		}
-		
-		/* LS: To avoid "Undefined" name in the label in the graph*/
+	
+		/* LS: Showing/hiding the element name  */
+        function getLabels(datasources) {
+			return datasources.map(function(item){
+                var isAttribute = /af:/.test(item);
+                var label = isAttribute ? item.match(/\w*\|.*$/)[0] : item.match(/\w+$/)[0];
+                if (!scope.config.includeElementName && (label.indexOf("|") !== -1)) {
+					label = label.split("|")[label.split("|").length - 1];
+				}
+				return {
+					Label: label
+				};
+			});	
+		}
+
+
+	
 		function updateLabel(data) {
 			labels=data.Rows.map (function(item) {
-				return item.Label;
+				var label = item.Label;
+				if (!scope.config.includeElementName && (label.indexOf("|") !== -1)) {
+					label = label.split("|")[label.split("|").length - 1];
+				}
+				return {
+					Label: label
+				};
+			
 			});
 		}
+
 		
 	/* LS:  Function dataUpdate	. To get all the data coming. 
 	First we check if the data is null
@@ -153,12 +193,33 @@
 		function dataUpdate(data) {
 			
 			if(!data) return;
-			if (data.Rows[0].Label) updateLabel(data);
+			if (!labels) {
+			labels = getLabels(scope.symbol.DataSources);
+			}
+			if (data.Rows[0].Label) {
+				/* LS: To avoid "Undefined" name in the label in the graph*/
+				updateLabel(data);
+			}
 			if (!labels || !chart) return;
 			
-			var dataprovider= convertToChart(data);
+			var dataprovider= convertToChart(data,labels);
 			
 			chart.dataProvider=dataprovider;
+			chart.depth3D=scope.config.depth;
+			chart.angle=scope.config.angle;
+			chart.graphs[0].columnWidth=scope.config.columnWidth;
+
+			chart.valueAxes[0].title=scope.config.title;
+			chart.valueAxes[0].title=scope.config.title;
+			chart.titles[0].text=scope.config.text;
+			if (scope.config.useFixedAxisRange) {
+						chart.valueAxes[0].minimum = scope.config.fixedYMin;
+						chart.valueAxes[0].maximum = scope.config.fixedYMax;
+			} else {
+						chart.valueAxes[0].minimum = undefined;
+						chart.valueAxes[0].maximum = undefined;
+			}
+			
 			chart.validateData();
 			
 		}
